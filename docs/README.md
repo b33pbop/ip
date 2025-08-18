eb6142cae0c3d023be4db27c564546a26ce50792
JL <djl010203@gmail.com>
2025-08-18 15:56:52 +0800

    1. Renamed chatbot to B33PBOP and updated all references of Duke to B33PBOP 2. Implemented skeletal version of B33PBOP: greets user and exits

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# B33PBOP User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/B33PBOP.java
@@ -0,0 +1,13 @@
+public class B33PBOP {
+    public static void main(String[] args) {
+        String horizontalLine = "_".repeat(50);
+        String greetings =
+                  horizontalLine + "\n"
+                + "Hello! I'm B33PBOP\n"
+                + "What can I do for you?\n"
+                + horizontalLine + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + horizontalLine + "\n";
+        System.out.println(greetings);
+    }
+}

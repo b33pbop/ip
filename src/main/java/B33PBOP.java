17240ce13c597b9b97f7c8a35474c39d0d6dc749
JL <djl010203@gmail.com>
2025-08-19 00:16:00 +0800

    Implemented echo functionality: When user types an input, the bot echos the input and exits only if user inputs bye

--- src/main/java/B33PBOP.java
+++ src/main/java/B33PBOP.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
+
 public class B33PBOP {
+    private static final String horizontalLine = "_".repeat(50);
+
     public static void main(String[] args) {
-        String horizontalLine = "_".repeat(50);
+        B33PBOP.greet();
+        B33PBOP.echo();
+    }
+
+    public static void greet() {
         String greetings =
-                  horizontalLine + "\n"
-                + "Hello! I'm B33PBOP\n"
-                + "What can I do for you?\n"
-                + horizontalLine + "\n"
-                + "Bye. Hope to see you again soon!\n"
+                horizontalLine + "\n"
+                + "I'm B33PBOP...\n"
+                + "What do you want?\n"
                 + horizontalLine + "\n";
         System.out.println(greetings);
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            String message =
+                    horizontalLine + "\n"
+                    + input + "\n"
+                    + horizontalLine + "\n";
+            System.out.println(message);
+
+            if (input.equalsIgnoreCase("bye")) {
+                String goodbyeMessage =
+                        horizontalLine + "\n"
+                        + "Please leave me alone\n"
+                        + horizontalLine + "\n";
+                System.out.println(goodbyeMessage);
+                break;
+            }
+        }
     }
 }

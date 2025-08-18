4b3de9b5ba71df97673db1308e5e26ddeb7e094c
JL <djl010203@gmail.com>
2025-08-19 01:03:29 +0800

    Added a new list functionality for users to add whatever they input. Created a Task class to represent each user input to be stored in my list

--- src/main/java/B33PBOP.java
+++ src/main/java/B33PBOP.java
@@ -1,40 +1,70 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class B33PBOP {
-    private static final String horizontalLine = "_".repeat(50);
+  private static final String horizontalLine = "_".repeat(50);
+  private static final List<Task> taskList = new ArrayList<>();
 
-    public static void main(String[] args) {
-        B33PBOP.greet();
-        B33PBOP.echo();
-    }
+  public static void main(String[] args) {
+    B33PBOP.greet();
+    B33PBOP.echo();
+  }
 
-    public static void greet() {
-        String greetings =
-                horizontalLine + "\n"
-                + "I'm B33PBOP...\n"
-                + "What do you want?\n"
-                + horizontalLine + "\n";
-        System.out.println(greetings);
-    }
-
-    public static void echo() {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String input = sc.nextLine();
-            String message =
-                    horizontalLine + "\n"
-                    + input + "\n"
+  public static void greet() {
+    String greetings =
+            horizontalLine + "\n"
+                    + "I'm B33PBOP...\n"
+                    + "What do you want?\n"
                     + horizontalLine + "\n";
-            System.out.println(message);
+    System.out.println(greetings);
+  }
 
-            if (input.equalsIgnoreCase("bye")) {
-                String goodbyeMessage =
-                        horizontalLine + "\n"
-                        + "Please leave me alone\n"
-                        + horizontalLine + "\n";
-                System.out.println(goodbyeMessage);
-                break;
-            }
-        }
+  public static void echo() {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      String input = sc.nextLine().trim().toLowerCase();
+      String response;
+
+      switch (input) {
+        case "bye":
+          response =
+                  horizontalLine + "\n"
+                          + "Please leave me alone\n"
+                          + horizontalLine + "\n";
+          System.out.println(response);
+          break;
+        case "list":
+          response =
+                  horizontalLine + "\n"
+                          + showTaskList()
+                          + horizontalLine + "\n";
+          System.out.println(response);
+          continue;
+        default:
+          response =
+                  horizontalLine + "\n"
+                          + "added: " + input + "\n"
+                          + horizontalLine + "\n";
+          addTask(input);
+          System.out.println(response);
+          continue;
+      }
+      break;
     }
+    sc.close();
+  }
+
+  public static void addTask(String taskName) {
+    Task newTask = new Task(taskName);
+    taskList.add(newTask);
+  }
+
+  public static String showTaskList() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < taskList.size(); i++) {
+      int idx = i + 1;
+      String task = idx + ". " + taskList.get(i) + "\n";
+      sb.append(task);
+    }
+    return sb.toString();
+  }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+  private final String taskName;
+  public static void main(String[] args) {}
+
+  public Task(String taskName) {
+    this.taskName = taskName;
+  }
+
+  public String toString() {
+    return taskName;
+  }
+}

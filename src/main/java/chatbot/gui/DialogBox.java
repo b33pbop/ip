6bf03318bf2ca14fa65d2dc486de7347f8b25797
JL <djl010203@gmail.com>
2025-09-19 12:15:53 +0800

    Update user profile and dialog box
    Dialog box did not stick to the side when width changes.
    User profile and dialog box just floats which looks bad.
    Update GUI such that moves with width change.

--- src/main/java/chatbot/gui/DialogBox.java
+++ src/main/java/chatbot/gui/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -37,6 +38,7 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        HBox.setHgrow(dialog, Priority.ALWAYS);
     }
 
     /**
